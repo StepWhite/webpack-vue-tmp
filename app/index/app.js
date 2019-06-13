53357a847ec9731fa8231a42bb202944de557b4c
engine063 <916529626@qq.com>
2019-06-13 15:14:09 +0800

    init

---
+++ app/index/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import Test from './views/test';
+
+new Vue({
+  el: '#app',
+  render: h => h(Test)
+});
---
+++ app/index/views/test.vue
@@ -0,0 +1,27 @@
+<style lang="less" scoped>
+div{
+  background: red;
+  p{
+    color: green;
+  }
+}
+</style>
+
+<template>
+  <div class='app-container'>
+    <div>
+      <p v-if="datas.length > 0" v-for="(item, index) in datas">{{item}}</p>
+    </div>
+    <button>BTN</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data() {
+      return {
+        datas: [1, 2, 3, 4]
+      }
+    }
+  }
+</script>
